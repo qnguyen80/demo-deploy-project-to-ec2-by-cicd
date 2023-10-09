f047e4a158c540759c7ae3db28b7d0ac56799841
Diep Sang <sdiep@MacBook-Pro-cua-Diep.local>
2023-10-09 11:17:31 +0700

    update code

--- app.js
+++ app.js
@@ -3,35 +3,18 @@ const app = express();
 const port = 3000;
 
 
-app.get('/', (req, res) => {
-    res.send("<h1>Demo deploy application to EC2 by CI/CD</h1><h4>Message: Success updated content</h4><p>Version: 0.1</p>");
-})
-
-app.get('/teams', (req, res) => {
-     res.send([
-         {
-             teamId: '1',
-             price: "free"
-         },
-         {
-             teamId: '2',
-             price: "200.000"
-         },
-         {
-             teamId: '3',
-             price: "240.000"
-         },
-         {
-             teamId: '4',
-             price: "280.000"
-         },
-         {
-             teamId: '5',
-             price: "320.000"
-         }
-     ]);
- })
-
- app.listen(port, () => {
-     console.log(`Application is up and listening to port: ${port}`);
- })
+const App = () => {
+ 
+    const history = useNavigate();
+   
+    return (
+      <div>
+        <div>
+          {/* <Router /> */}
+          <h1>AWS Team 4</h1>
+        </div>
+      </div>
+    );
+  };
+   
+  export default App;
