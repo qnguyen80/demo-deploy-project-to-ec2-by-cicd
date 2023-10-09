fd90d07e3308e0549f29eb95785273e921c82a46
qnguyen80 <qnguyen80@dxc.com>
2023-10-09 10:21:16 +0700

    update

--- scripts/application_stop.sh
+++ scripts/application_stop.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #Stopping existing node servers
-echo "Stopping any existing node servers"
+echo "Stopping any existing node servers test"
 # sudo pkill node
 echo "sudo kill -9 node"
 sudo kill -9 node
