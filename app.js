2da44a1843d12b348e0da8ec11cf684fdde9a410
Diep Sang <sdiep@MacBook-Pro-cua-Diep.local>
2023-10-09 09:25:30 +0700

    update app.js

--- app.js
+++ app.js
@@ -2,35 +2,45 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-    res.send("<h1>Demo deploy application to EC2 by CI/CD</h1><h4>Message: Success update content</h4><p>Version: 0.1</p>");
-})
+// app.get('/', (req, res) => {
+//     res.send("<h1>Demo deploy application to EC2 by CI/CD</h1><h4>Message: Success update content</h4><p>Version: 0.1</p>");
+// })
 
-app.get('/teams', (req, res) => {
-    res.send([
-        {
-            teamId: '1',
-            price: "free"
-        },
-        {
-            teamId: '2',
-            price: "200.000"
-        },
-        {
-            teamId: '3',
-            price: "240.000"
-        },
-        {
-            teamId: '4',
-            price: "280.000"
-        },
-        {
-            teamId: '5',
-            price: "320.000"
-        }
-    ]);
-})
+// app.get('/teams', (req, res) => {
+//     res.send([
+//         {
+//             teamId: '1',
+//             price: "free"
+//         },
+//         {
+//             teamId: '2',
+//             price: "200.000"
+//         },
+//         {
+//             teamId: '3',
+//             price: "240.000"
+//         },
+//         {
+//             teamId: '4',
+//             price: "280.000"
+//         },
+//         {
+//             teamId: '5',
+//             price: "320.000"
+//         }
+//     ]);
+// })
 
-app.listen(port, () => {
-    console.log(`Application is up and listening to port: ${port}`);
-})
+// app.listen(port, () => {
+//     console.log(`Application is up and listening to port: ${port}`);
+// })
+
+export default function App(){
+    return (
+        <>
+        <div>
+            <h1>AWS Team 4</h1>
+        </div>
+        </>
+    )
+}
