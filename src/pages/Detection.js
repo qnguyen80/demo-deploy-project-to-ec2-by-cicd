0826aeaee636a08036e6f5cbff460d8159ed6cea
Diep Sang <sdiep@192.168.1.122>
2023-10-10 15:16:21 +0700

    update code

--- src/pages/Detection.js
+++ src/pages/Detection.js
@@ -1,9 +1,31 @@
 import * as React from 'react';
+import { useEffect } from 'react';
+import axios from 'axios';
+import CircularProgress from '@mui/material/CircularProgress';
+import Box from '@mui/material/Box';
 
 export default function Detection() {
+    const [ip, setIP] = React.useState('');
+    const getData = async () => {
+        const res = await axios.get('https://geolocation-db.com/json/');
+        setIP(res.data.IPv4);
+    }
+
+    useEffect(() => {
+        getData();
+    }, []);
     return (
         <>
-            <div>Detection Page</div>
+            <h2>Your IPv4 Address is:</h2>
+            {ip && (
+               <h4>{ip}</h4>
+            )}
+            {!ip && (
+                <Box sx={{ display: 'flex' }}>
+                    <CircularProgress />
+                </Box>
+            )}
+
         </>
     );
 }
--- src/pages/S3.js
+++ src/pages/S3.js
@@ -1,9 +1,12 @@
 import * as React from 'react';
 
 export default function S3() {
+    const s3Url = "https://aws-s3-image.s3.ap-southeast-2.amazonaws.com/";
+    const imgKey = "image-background-demo.jpg";
     return (
         <>
-            <div>S3 Page</div>
+            <h4>Image data store in S3</h4>
+            <img src={s3Url+imgKey} />
         </>
     );
 }
